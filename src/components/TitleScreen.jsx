e1ff4ed23bb4b5a94655ab8da6e89f401dc64160
Lining Ao <Nicolea25418@gmail.com>
2025-06-18 20:53:22 -0400

    pixel button component

--- src/components/TitleScreen.jsx
+++ src/components/TitleScreen.jsx
@@ -1,13 +1,11 @@
 import React from "react";
+import PixelButton from "./PixelButton";
 
 const TitleScreen = ({ onStart }) => {
   return (
     <div className="title-screen-container">
       <h1 style={{ width: "50%", textAlign: "center" }}>The Fading Tail</h1>
       <p className="pulse-text">Meowric's Adventures</p>
-      <h2>
-        <a onClick={onStart}>Click to start</a>
-      </h2>
     </div>
   );
 };
